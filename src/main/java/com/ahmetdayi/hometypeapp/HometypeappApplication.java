446816c3fe55a17a4f98db78d733c7dc552c1c97
Ahmet Dayi <ahmet_dayi@eskisehir.edu.tr>
2023-02-03 19:24:02 +0300

    entities created

---
+++ src/main/java/com/ahmetdayi/hometypeapp/HometypeappApplication.java
@@ -0,0 +1,13 @@
+package com.ahmetdayi.hometypeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HometypeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HometypeappApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ahmetdayi/hometypeapp/entity/Summery.java
@@ -0,0 +1,4 @@
+package com.ahmetdayi.hometypeapp.entity;
+
+public class Summery {
+}
