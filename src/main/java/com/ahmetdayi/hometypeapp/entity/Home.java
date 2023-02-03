f87476383c8b9f8ebd338d2171f18368fb3e2af8
Ahmet Dayi <ahmet_dayi@eskisehir.edu.tr>
2023-02-03 19:40:58 +0300

    entity features added

--- src/main/java/com/ahmetdayi/hometypeapp/entity/Home.java
+++ src/main/java/com/ahmetdayi/hometypeapp/entity/Home.java
@@ -1,4 +1,41 @@
 package com.ahmetdayi.hometypeapp.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+//Lombok
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+//persistence
+@Entity
 public class Home {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Min(1)
+    @NotNull
+    private double price;
+
+    @Min(1)
+    @NotNull
+    private double houseSize;
+
+    @NotNull
+    private int roomCount;
+
+    @NotNull
+    private int livingRoomCount;
+
 }
--- src/main/java/com/ahmetdayi/hometypeapp/entity/Villa.java
+++ src/main/java/com/ahmetdayi/hometypeapp/entity/Villa.java
@@ -1,4 +1,39 @@
 package com.ahmetdayi.hometypeapp.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+//Lombok
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+//persistence
+@Entity
 public class Villa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotNull
+    @Min(1)
+    private double price;
+
+    @NotNull
+    @Min(1)
+    private double villaSize;
+
+    @NotNull
+    private int roomCount;
+    @NotNull
+    private int livingRoomCount;
 }
